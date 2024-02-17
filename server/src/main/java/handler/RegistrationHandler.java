5284b72eb2db8044f41d79a57d01915a358734f2
apd915 <apd915@gmail.com>
2024-02-17 13:55:40 -0700

    Created different handlers to handle servers

---
+++ server/src/main/java/handler/RegistrationHandler.java
@@ -0,0 +1,13 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class RegistrationHandler implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import handler.RegistrationHandler;
 import spark.*;
 
 public class Server {
@@ -10,6 +11,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", new RegistrationHandler());
 
         Spark.awaitInitialization();
         return Spark.port();
